797d166b1b2d87536ea26345d2cba4cb5f723ca0
jluis-imac <skateluis27@hotmail.com>
2025-02-19 19:34:00 -0600

    feat: eglobal challenge

---
+++ src/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex flex-column ga-5 pa-6">
+    <h2>Eglobal Challenge</h2>
+    <v-tabs v-model="tab" bg-color="primary">
+      <v-tab value="one"> Consulta </v-tab>
+      <v-tab value="two"> Edición </v-tab>
+      <v-tab value="three"> Registro </v-tab>
+    </v-tabs>
+    <v-card-text>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="one">
+          <Table />
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="two">
+          <Form />
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="three">
+          <Form />
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card-text>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Table from "@/components/Table.vue";
+import Form from "@/components/Form.vue";
+
+const tab = ref();
+</script>
